99ad3ee3a1b5ab5242c519478fd31aaaca0e1417
Funi <163131720+ST10438308@users.noreply.github.com>
2025-05-23 20:42:10 +0200

    Added UserProfile.cs to store user name and topic history

---
+++ CyberSecurityChatbot/UserProfile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberSecurityChatbot
+{
+    class UserProfile
+    {
+        public string Name { get; set; }
+        public int? Age { get; set; }
+        public string Role { get; set; } // e.g., "Student", "Professor"
+        public List<string> Interests { get; set; } = new List<string>();
+
+        public void AddInterest(string topic)
+        {
+            if(!Interests.Contains(topic.ToLower()))
+            {
+                Interests.Add(topic.ToLower());
+            }
+        }
+    }
+}
